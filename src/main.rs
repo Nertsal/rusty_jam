845a523988cc3b4d6d4503ab665a860f5154f3c0
Alexander <sasha.kudasov04@gmail.com>
2021-08-21 21:58:35 +0500

    Setup project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+// #![windows_subsystem = "windows"]
+use macroquad::prelude::*;
+use std::time::Instant;
+
+mod components;
+mod constants;
+mod game;
+mod resources;
+mod systems;
+
+use game::Game;
+
+const FIXED_DELTA_TIME: f32 = 1.0 / 60.0;
+
+#[macroquad::main("Rusty Jam")]
+async fn main() {
+    let mut game = Game::new();
+
+    // let mut game = Game::new().await;
+    let mut frame_time = 0.0;
+    loop {
+        println!("---- next frame ----");
+        let delta_time = get_frame_time();
+        frame_time += delta_time;
+        let time = Instant::now();
+        game.update(delta_time);
+        println!("update: {}ms", time.elapsed().as_millis());
+
+        let time = Instant::now();
+        let mut frames = 0;
+        while frame_time >= FIXED_DELTA_TIME {
+            game.fixed_update(FIXED_DELTA_TIME);
+            frame_time -= FIXED_DELTA_TIME;
+            frames += 1;
+        }
+        println!(
+            "fixed_update: {}ms / {} frames",
+            time.elapsed().as_millis(),
+            frames
+        );
+
+        let time = Instant::now();
+        game.draw();
+        println!("draw: {}ms", time.elapsed().as_millis());
+        next_frame().await;
+    }
 }
